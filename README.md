06583ac3f298f05b38a1d50ee4db922ae8dd8d4b
Oliver-Mutuku <olivermulei001@gmail.com>
2024-05-18 17:29:34 +0300

    Finished the basic logic for the project. Implemented logic to compare both computer and player/user choices, then increment the respective points for the winner.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Rock-Paper_Scissors
+# Rock-Paper-Scissors
+
+
+# Computer choice function
+- Get three random numbers (one for rock, one for paper and ane for scissors)
+- return one random number of the three
+- choice 0 = rock
+- choice 1 = paper
+- choice 2 = scissors
+
+
+# Human choice function
+- prompt the user(player) to choose one of the three choices of the game
+- if the choice is typed incorrectly, prompt the user again for their choice
+- return the player's choice
+
+
+# Play function
+- declare 2 variables; each to store the respective return values of the 2 above functions
+- compare both variables and determine the winner
+- Iterate comparing the choices 5 times, then declare the winner
+
+
+
+
---
+++ script.js
@@ -0,0 +1,90 @@
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+
+// Using recursion(function calling itself). 
+// This should however be used judiciously because it can potentially lead to stack overflow errors
+
+// function humanChoice () {
+//     let playerChoice = prompt("Choose one of the options: rock / paper / scissors");
+//     if (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
+//         return humanChoice();
+//     } else {
+//         return playerChoice;
+//     }
+// }
+// console.log(humanChoice())
+
+
+function getHumanChoice() {
+    let playerChoice;
+    do {
+        playerChoice = prompt("Choose one of the options: rock / paper / scissors").toLowerCase();
+    } while (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors');
+    return playerChoice;
+}
+
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+function playRound (computerChoice, humanChoice) {
+
+        if (computerChoice === humanChoice) {
+            computerScore += 1;
+            humanScore += 1;
+            console.log(`That was a draw! Scores increment by one for each. Computer Score is now ${computerScore}. Your score is ${humanScore}.`);
+
+        } else if (computerChoice === 'rock' && humanChoice === 'scissors' ||
+                    computerChoice === 'paper' && humanChoice === 'rock' ||
+                    computerChoice == 'scissors' && humanChoice === 'paper'
+        ) {
+            computerScore++;
+            console.log(`Computer wins. ${computerChoice} beats ${humanChoice}. Computer Score is now ${computerScore}. Your score is ${humanScore}.`);
+        }
+        else {
+            humanScore++;
+            console.log(`You win. Computer chose ${computerChoice}. ${humanChoice} beats ${computerChoice}. Computer Score is now ${computerScore}. Your score is ${humanScore}.`);
+        }
+        return (computerScore, humanScore);
+    
+}
+
+
+// console.log(playRound(humanSelection, computerSelection))
+
+function playGame () {
+    let count = 0;
+    while (count < 5) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection)
+        count++
+    }
+    console.log("GAME OVER");
+    if (computerScore === humanScore) {
+        alert("Turns out that was a Draw. Good game");
+    }
+    else if (computerScore > humanScore) {
+        alert(`Computer won by ${computerScore} to ${humanScore}`);
+    } else {
+        alert(`You won by ${humanScore} to ${computerScore}`);
+    }
+}
+
+
+playGame();
+
+
+
+
